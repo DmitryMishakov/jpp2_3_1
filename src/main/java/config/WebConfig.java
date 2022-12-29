34ddd12f3c1d3801ee67d898c6db399b63b39ff5
Dmitry Mishakov <welzevul0@gmail.com>
2022-12-29 20:24:17 +0300

    added db configs and hibernate

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -1,9 +1,18 @@
 package config;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,15 +20,27 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan("java")
+@PropertySource("classpath:db.properties")
+@EnableTransactionManagement
 public class WebConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
 
-    public WebConfig(ApplicationContext applicationContext) {
+    private final Environment environment;
+
+    private Logger logger = LoggerFactory.getLogger(WebConfig.class.getName());
+
+    public WebConfig(ApplicationContext applicationContext, Environment environment) {
         this.applicationContext = applicationContext;
+        this.environment = environment;
     }
 
     @Bean
@@ -46,4 +67,42 @@ public class WebConfig implements WebMvcConfigurer {
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
     }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("database.driver_class"));
+        dataSource.setUrl(environment.getRequiredProperty("database.connection.url"));
+        dataSource.setUsername(environment.getRequiredProperty("database.connection.username"));
+        dataSource.setPassword(environment.getRequiredProperty("database.connection.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean =
+                new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean
+                .setPackagesToScan(environment.getRequiredProperty("database.entity.package"));
+
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactoryBean.setJpaProperties(getHibernateProperties());
+
+        return entityManagerFactoryBean;
+    }
+
+    private Properties getHibernateProperties() {
+        try {
+            Properties properties = new Properties();
+            InputStream inputStream = getClass().getClassLoader()
+                    .getResourceAsStream("hibernate.properties");
+            properties.load(inputStream);
+            return properties;
+        } catch (IOException ex) {
+            logger.error("can't find property file", new IllegalArgumentException(ex));
+        }
+        return null;
+    }
+
 }
