c5818062a2d00b07589ec092417363fcb494f99a
Dmitry Mishakov <welzevul0@gmail.com>
2023-01-31 13:14:07 +0300

    fix: finally fixed all bugs

--- src/main/java/com/mishakov/controller/StartController.java
+++ src/main/java/com/mishakov/controller/StartController.java
@@ -1,16 +1,14 @@
 package com.mishakov.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/start")
+@RequestMapping("/")
 public class StartController {
 
-    @RequestMapping(value = "/get", method = RequestMethod.GET)
-    @ResponseBody
+    @GetMapping()
     public String getStart() {
         return "index";
     }
